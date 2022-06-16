8e0ca9de94fbb5443bec09912ab3147661422222
Pedro do Amaral Rangel <107447016+PedrimAr@users.noreply.github.com>
2022-06-16 13:56:06 -0300

    Update vogais.c

--- vogais.c
+++ vogais.c
@@ -1,3 +1,5 @@
+//Aluno: Pedro do Amaral Rangel
+
 //A intenção do seguinte programa é dizer quantas vogais tem em um nome.
 
 #include <stdio.h>
@@ -8,7 +10,7 @@ void main() { // Aqui é criada a função main, do tipo void.
     char nome[100]; // Aqui é declarado a string chamada nome.
     printf("Digite seu nome: "); // Aqui é printada a mensagem para que o nome seja digitado.
     gets(nome); // Aqui é atribuído conteúdo à string nome, oriundo do teclado.
-    for(count = 0; count < 100; count++) { // Aqui é iniciado o for
+    for(count = 0; nome[count] != '\0'; count++) { // Aqui é iniciado o for
         if(nome[count] == 'a' || nome[count] == 'e' || nome[count] == 'i' || nome[count] == 'o' || nome[count] == 'u'){ // Aqui é verificado se há vogal
             vogais += 1; // Aqui é adicionado 1 há variável vogais quando é encontrada uma vogal
         }
